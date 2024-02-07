13238cbae219fbf16665836f5b0c78219e8e2490
vaishnavi.borse <borsevr13+github@gmail.com>
2024-02-07 14:21:05 +0530

    adding application to github

---
+++ Bulky/BulkyWeb/Controllers/CategoryController1.cs
@@ -0,0 +1,29 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+
+{
+
+    //private readonly ApplicationDbContext _db;
+
+    public class CategoryController1 : Controller
+    {
+
+        public CategoryController1(ApplicationDbContext db)
+        {
+            //_db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SaveDataToCategory()
+        {
+            //var categoryObj;
+            // adding changes
+            return Ok();
+        }
+    }
+}
---
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyWeb.Models;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories {get; set; }
+        public DbSet<BulkyWeb.Models.Category> Category { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+        
+    }
+}
